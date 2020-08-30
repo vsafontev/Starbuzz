7748bcdaa4aa245b39b4d0d939446a56c0903fb0
vsafontev <vladislavsafontev@gmail.com>
2020-08-30 14:36:15 +0300

    commit 5

--- app/src/main/java/com/example/starbuzz/TopLevelActivity.java
+++ app/src/main/java/com/example/starbuzz/TopLevelActivity.java
@@ -2,7 +2,11 @@ package com.example.starbuzz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 public class TopLevelActivity extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class TopLevelActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_level);
+
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> listView,
+                                    View itemView,
+                                    int position,
+                                    long id) {
+                if (position == 0) {
+                    Intent intent = new Intent(TopLevelActivity.this, DrinkCategoryActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        //Добавить слушателя к списковому представлению
+        ListView listView = (ListView) findViewById(R.id.list_options);
+        listView.setOnItemClickListener(itemClickListener);
     }
+
+
 }
