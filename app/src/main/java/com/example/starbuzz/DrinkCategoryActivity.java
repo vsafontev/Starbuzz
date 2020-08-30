770dc910dbd30dca20155299da32465529559db7
vsafontev <vladislavsafontev@gmail.com>
2020-08-30 15:30:59 +0300

    commit 6

--- app/src/main/java/com/example/starbuzz/DrinkCategoryActivity.java
+++ app/src/main/java/com/example/starbuzz/DrinkCategoryActivity.java
@@ -3,6 +3,8 @@ package com.example.starbuzz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class DrinkCategoryActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class DrinkCategoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink_category);
+        ArrayAdapter<Drink> listAdapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                Drink.drinks);
+        ListView listDrinks = (ListView) findViewById(R.id.list_drinks);
+        listDrinks.setAdapter(listAdapter);
     }
 }
